774ca11d521527ffb08746ced4c6a7fdb21ac53d
Rakibul Islam <rakibul.rk2@gmail.com>
2025-05-24 11:21:50 +0600

    up

--- portfolio/mainapp/urls.py
+++ portfolio/mainapp/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import *
 urlpatterns = [
-    path('', book_list, name='book_list'),
+    path('index', book_list, name='book_list'),
     path('add/',book_create, name='book_create'),
     path('edit/<int:pk>/', book_update, name='book_update'),
     path('delete/<int:pk>/',book_delete, name='book_delete'),
